77e03702beb5ccb70b61f359dc7eb6e47d9ad19b
Ruben FB <rubenfb14@gmail.com>
2019-03-26 22:19:48 +0100

    add a config file

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+import json
+
 SEED = 1122
 NUM_THREADS = 4
 
@@ -6,3 +8,23 @@ DATA_PATH = 'img_align_celeba/'
 CHKPTS_PATH = 'checkpoints/'
 GRAPHS_PATH = 'graphs/'
 OUTPUT_PATH = 'output/'
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+CONFIG = config['default_config']
+
+DIM_X = CONFIG['output']['dimX']
+DIM_Y = CONFIG['output']['dimY'] 
+DIM_Z = CONFIG['output']['dimZ'] 
+
+BATCH_SIZE = CONFIG['input']['batch_size']
+Z_NOISE_DIM = CONFIG['input']['batch_size'] 
+
+NUM_EPOCHS = CONFIG['training']['num_epochs']
+LEARNING_RATE = CONFIG['training']['learning_rate']
+BETA1 = CONFIG['training']['beta1']
+
+SAVE_MODEL_EVERY = CONFIG['save_model_every']
+SAVE_EXAMPLE_EVERY = CONFIG['save_example_every']
+PRINT_INFO_EVERY = CONFIG['print_info_every']
--- histology_main.py
+++ histology_main.py
@@ -1,46 +1,37 @@
 import tensorflow as tf
 import data_processing as data
 import model
+import numpy as np
 import output
-from constants import BASE_PATH, DATA_PATH, CHKPTS_PATH, GRAPHS_PATH, SEED, NUM_THREADS
+from constants import *
+import argparse
+import configparser
+
 
 if __name__ == '__main__':
-    
-    # Dimensions of data
-    dim1 = 128 
-    dim2 = 128 
-    dim3 = 3 
-    batch_size = 16
-    #noise input vector
-    z_dim = 100 
-    
-    # Number of epochs to run
-    num_epochs = 1000 
-    learning_rate = 0.00025
-    beta1 = 0.45
-    save_model_every = 100
-    save_example_every = 10
-    print_every = 100
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--z_file", help="z_noise_file")
+    args = parser.parse_args()
 
     with tf.Graph().as_default() as graph1:
         # Random seed for reproducibility
         tf.set_random_seed(SEED)
         
         # Placeholders
-        z_batch_tensor = tf.random.uniform((batch_size, z_dim),-1, 1, dtype=tf.float32)
-        is_training = tf.placeholder(tf.bool, shape=1)
+        z_batch_tensor = tf.random.normal((BATCH_SIZE, Z_NOISE_DIM),0, 2, dtype=tf.float32)
 
         # Dataset
-        dataset = data.create_dataset(BASE_PATH + DATA_PATH, batch_size, dim1, dim2, dim3, num_epochs)
+        dataset = data.create_dataset(BASE_PATH + DATA_PATH, BATCH_SIZE, DIM_X, DIM_Y, DIM_Z, NUM_EPOCHS)
         iterator = dataset.make_initializable_iterator()
         next_batch = iterator.get_next()
         saveable_obj = tf.data.experimental.make_saveable_from_iterator(iterator)
 
         # Model
-        input_real, input_z, _ = model.model_inputs(dim1, dim2, dim3, z_dim)
+        input_real, input_z, _ = model.model_inputs(DIM_X, DIM_Y, DIM_Z, Z_NOISE_DIM)
         is_training = tf.placeholder(dtype=tf.bool, name='is_training')
-        d_loss, g_loss = model.model_loss(input_real, input_z, dim3, is_training)
-        d_train_opt, g_train_opt = model.model_opt(d_loss, g_loss, learning_rate, beta1)
+        d_loss, g_loss = model.model_loss(input_real, input_z, DIM_Z, is_training)
+        d_train_opt, g_train_opt = model.model_opt(d_loss, g_loss, LEARNING_RATE, BETA1)
 
         steps = 0
         saver = tf.train.Saver()
@@ -55,18 +46,28 @@ if __name__ == '__main__':
                 try:
                     # Run optimizers
                     batch = sess.run(next_batch)
-                    batch_z = sess.run(z_batch_tensor)
+                    #when a noise file is specified
+                    if (args.z_file != None):
+                        z_file = open(BASE_PATH + OUTPUT_PATH + args.z_file,"r")
+                        z_file_data = z_file.read().strip()
+                        z_data = z_file_data.split(" ")
+                        z_data = np.array(z_data).astype(np.float).reshape(BATCH_SIZE,Z_NOISE_DIM)
+                        output.save_single_output(sess,z_data,input_z,is_training,steps)
+                        break
+                    #otherwise training mode
+                    else:
+                        batch_z = sess.run(z_batch_tensor)
                     sess.run(d_train_opt, feed_dict={input_real: batch, input_z: batch_z,  is_training : True})
                     sess.run(g_train_opt, feed_dict={input_z: batch_z, is_training : True})
-                    writer = tf.summary.FileWriter(BASE_PATH + GRAPHS_PATH, sess.graph)
-                    if steps % save_model_every == 0:
+                    #writer = tf.summary.FileWriter(BASE_PATH + GRAPHS_PATH, sess.graph)
+                    if steps % SAVE_MODEL_EVERY == 0:
                         saver.save(sess, BASE_PATH + CHKPTS_PATH + "model.ckpt")
-                    if steps % print_every == 0:
+                    if steps % PRINT_INFO_EVERY == 0:
                         train_loss_d = d_loss.eval({input_real: batch, input_z: batch_z,is_training : True}, session = sess)
                         train_loss_g = g_loss.eval({input_z: batch_z,is_training : True}, session = sess)
                         print("Epoch Step {}...".format(steps),"Discriminator Loss: {:.4f}...".format(train_loss_d),"Generator Loss: {:.4f}".format(train_loss_g))
-                    if steps % save_example_every == 0:
-                        output.save_output(sess,z_batch_tensor,input_z,is_training,steps,dim1,dim2,dim3)
+                    if steps % SAVE_EXAMPLE_EVERY == 0:
+                        output.save_output(sess,z_batch_tensor,input_z,is_training,steps)
                         
                 except tf.errors.OutOfRangeError:
                     print("End")
