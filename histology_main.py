a8259168bbfbb1caf629a486f74b9e3552236c46
Ruben FB <rubenfb14@gmail.com>
2019-03-28 01:06:23 +0100

    changes in the way we save and restore from checkpoints

--- histology_main.py
+++ histology_main.py
@@ -25,7 +25,6 @@ if __name__ == '__main__':
         dataset = data.create_dataset(BASE_PATH + DATA_PATH, BATCH_SIZE, DIM_X, DIM_Y, DIM_Z, NUM_EPOCHS)
         iterator = dataset.make_initializable_iterator()
         next_batch = iterator.get_next()
-        saveable_obj = tf.data.experimental.make_saveable_from_iterator(iterator)
 
         # Model
         input_real, input_z, _ = model.model_inputs(DIM_X, DIM_Y, DIM_Z, Z_NOISE_DIM)
@@ -33,19 +32,24 @@ if __name__ == '__main__':
         d_loss, g_loss = model.model_loss(input_real, input_z, DIM_Z, is_training)
         d_train_opt, g_train_opt = model.model_opt(d_loss, g_loss, LEARNING_RATE, BETA1)
 
-        steps = 0
-        saver = tf.train.Saver()
+        global_step = tf.Variable(0, trainable=False, name='global_step')
+        increment_global_step = tf.assign_add(global_step,1, name = 'increment_global_step')
+        saver = tf.train.Saver(max_to_keep = 20, keep_checkpoint_every_n_hours=2)
+
         with tf.Session() as sess:
             try:
-                saver.restore(sess, BASE_PATH + CHKPTS_PATH + "model.ckpt")
+                saver.restore(sess, tf.train.latest_checkpoint(BASE_PATH + CHKPTS_PATH))                
+                last_global_step = sess.run(global_step)
+                print(last_global_step)
             except:
+             
                 sess.run(tf.global_variables_initializer())
             sess.run(iterator.initializer)
             while True:
-                steps += 1
                 try:
                     # Run optimizers
                     batch = sess.run(next_batch)
+                    steps = sess.run(global_step)
                     #when a noise file is specified
                     if (args.z_file != None):
                         z_file = open(BASE_PATH + OUTPUT_PATH + args.z_file,"r")
@@ -57,11 +61,13 @@ if __name__ == '__main__':
                     #otherwise training mode
                     else:
                         batch_z = sess.run(z_batch_tensor)
+                        
                     sess.run(d_train_opt, feed_dict={input_real: batch, input_z: batch_z,  is_training : True})
                     sess.run(g_train_opt, feed_dict={input_z: batch_z, is_training : True})
+                    sess.run(increment_global_step)
                     #writer = tf.summary.FileWriter(BASE_PATH + GRAPHS_PATH, sess.graph)
                     if steps % SAVE_MODEL_EVERY == 0:
-                        saver.save(sess, BASE_PATH + CHKPTS_PATH + "model.ckpt")
+                        saver.save(sess, BASE_PATH + CHKPTS_PATH + "model", global_step = global_step)
                     if steps % PRINT_INFO_EVERY == 0:
                         train_loss_d = d_loss.eval({input_real: batch, input_z: batch_z,is_training : True}, session = sess)
                         train_loss_g = g_loss.eval({input_z: batch_z,is_training : True}, session = sess)
