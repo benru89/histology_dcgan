cab0492813bdc3828540b8d9e44453628e5b4f3f
Ruben FB <rubenfb14@gmail.com>
2019-04-05 14:42:58 +0200

    extract patches of size 128 instead of resizing the whole image

--- data_processing.py
+++ data_processing.py
@@ -7,14 +7,25 @@ from PIL import Image
 import numpy as np
 from constants import SEED, NUM_THREADS
 
-def read_normalize_resize_image(filename, img_height, img_width, channels):
+def extract_patches(image, num_patches=10, patch_size=128):
+    """Get `num_patches` random crops from the image"""
+    patches = []
+    for i in range(num_patches):
+        patch = tf.random_crop(image, [patch_size, patch_size, 3])
+        patches.append(patch)
+
+    patches = tf.stack(patches)
+    assert patches.get_shape().dims == [num_patches, patch_size, patch_size, 3]
+    return patches
+
+
+def read_image(filename, channels):
     """
     This function does blah blah.
     """
     image_string = tf.read_file(filename)
     image = tf.image.decode_jpeg(image_string, channels=channels)
     image = tf.image.convert_image_dtype(image, tf.float32)
-    image = tf.image.resize_images(image, [img_height, img_width])
     return image
 
 
@@ -24,7 +35,7 @@ def train_preprocess(image):
     """
     image = tf.image.random_flip_left_right(image)
     image = tf.image.random_flip_up_down(image)
-    image = tf.image.rot90(image, np.random.randint(0, 3))
+    image = tf.image.rot90(image, tf.random_uniform(shape=[], minval=0, maxval=4, dtype=tf.int32))
     #image = tf.image.random_brightness(image, max_delta=32.0 / 255.0)
     #image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
     image = tf.clip_by_value(image, 0.0, 1.0)
@@ -38,15 +49,20 @@ def create_dataset(path, batch_size, img_height, img_width, channels, num_epochs
     with tf.device('/cpu:0'):
         convert_tiff_to_jpeg(path)
         filenames = glob(os.path.join(path, "*.jpg"))
-        dataset = tf.data.Dataset.from_tensor_slices((filenames))
-        dataset = dataset.repeat(num_epochs)
-        dataset = dataset.shuffle(buffer_size=len(filenames), seed=SEED)
-        dataset = dataset.map(lambda filename: read_normalize_resize_image(
-            filename, img_height, img_width, channels), num_parallel_calls=NUM_THREADS)
-        dataset = dataset.map(train_preprocess, num_parallel_calls=NUM_THREADS)
-        dataset = dataset.batch(batch_size)
-        dataset = dataset.prefetch(1)
-        return dataset
+        dataset = (tf.data.Dataset.from_tensor_slices((filenames))
+               .repeat(num_epochs)
+               .shuffle(buffer_size=len(filenames))
+               .map(lambda filename: read_image(
+                   filename, channels), num_parallel_calls=NUM_THREADS)
+               .map(lambda image: extract_patches(
+                   image, num_patches=20, patch_size=img_height), num_parallel_calls=NUM_THREADS)
+               .map(train_preprocess, num_parallel_calls=NUM_THREADS)
+               .apply(tf.data.experimental.unbatch())
+               .shuffle(buffer_size=len(filenames))
+               .batch(batch_size)
+               .prefetch(1))
+
+    return dataset
 
 def convert_tiff_to_jpeg(path):
     """
