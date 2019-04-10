bafbd4f7685dd43b2ee54cbcc47522becb6a47fd
Ruben FB <rubenfb14@gmail.com>
2019-04-10 22:27:10 +0200

    different learning rate for g and d

--- histology_main.py
+++ histology_main.py
@@ -4,11 +4,12 @@ import tensorflow as tf
 import numpy as np
 import data_processing as data
 import model
+import gc
 import output
 from constants import (SEED, BATCH_SIZE, Z_NOISE_DIM, DIM_X, DIM_Y, DIM_Z, NUM_EPOCHS,
-                       BASE_PATH, DATA_PATH, LEARNING_RATE, BETA1, OUTPUT_PATH, SAVE_MODEL_EVERY,
+                       BASE_PATH, DATA_PATH, D_LEARNING_RATE, G_LEARNING_RATE, BETA1, OUTPUT_PATH, SAVE_MODEL_EVERY,
                        PRINT_INFO_EVERY, CHKPTS_PATH, SAVE_EXAMPLE_EVERY)
-
+import matplotlib.pyplot as plt
 
 def run():
     """
@@ -23,7 +24,7 @@ def run():
         tf.set_random_seed(SEED)
 
         # Placeholders
-        z_batch_tensor = tf.random.normal((BATCH_SIZE, Z_NOISE_DIM), 0, 2, dtype=tf.float32)
+        z_batch_tensor = tf.random.normal((BATCH_SIZE, Z_NOISE_DIM), dtype=tf.float32)
 
         # Dataset
         dataset = data.create_dataset(BASE_PATH + DATA_PATH, BATCH_SIZE, DIM_X, DIM_Y, DIM_Z, NUM_EPOCHS)
@@ -33,7 +34,7 @@ def run():
         input_real, input_z, _ = model.model_inputs(DIM_X, DIM_Y, DIM_Z, Z_NOISE_DIM)
         is_training = tf.Variable(True, name='is_training')
         d_loss, g_loss = model.model_loss(input_real, input_z, DIM_Z, is_training)
-        d_train_opt, g_train_opt = model.model_opt(d_loss, g_loss, LEARNING_RATE, BETA1)
+        d_train_opt, g_train_opt = model.model_opt(d_loss, g_loss, D_LEARNING_RATE, G_LEARNING_RATE, BETA1)
 
         global_step = tf.Variable(0, trainable=False, name='global_step')
         increment_global_step = tf.assign_add(global_step, 1, name='increment_global_step')
@@ -53,6 +54,18 @@ def run():
                 try:
                     # Run optimizers
                     batch = sess.run(next_batch)
+                    #uncomment to see how the images in the batch look like
+                    """imgs = [img[:, :, :] for img in batch]
+                    figure_side = 8
+                    fig, ax = plt.subplots(nrows=figure_side, ncols=figure_side, figsize=(40, 40))
+                    k = 0
+                    for i in range(figure_side):
+                        for j in range(figure_side):
+                            ax[i, j].imshow((np.reshape(imgs[k], (DIM_X, DIM_Y, DIM_Z))*255).astype(np.uint8))
+                            ax[i, j].axis('off')
+                            k = k+1
+                    fig.subplots_adjust(hspace=0.01, wspace=0.01)
+                    plt.show()"""
                     steps = sess.run(global_step)
                     #when a noise file is specified
                     if args.z_file is not None:
@@ -75,6 +88,7 @@ def run():
                     if steps % PRINT_INFO_EVERY == 0:
                         train_loss_d = d_loss.eval({input_real: batch, input_z: batch_z, is_training : True}, session=sess)
                         train_loss_g = g_loss.eval({input_z: batch_z, is_training : True}, session=sess)
+                        gc.collect()
                         print("Epoch Step {}...".format(steps),
                               "Discriminator Loss: {:.4f}...".format(train_loss_d),
                               "Generator Loss: {:.4f}".format(train_loss_g))
