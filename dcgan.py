ddbfcbaf8361610c6f83599c441856074b3f20b8
Ruben FB <rubenfb14@gmail.com>
2019-04-10 22:25:02 +0200

    use relu in generator instead of lrelu

--- dcgan.py
+++ dcgan.py
@@ -1,11 +1,8 @@
 import tensorflow as tf
 
-def lrelu(x):
-    return tf.maximum(x, tf.multiply(x, 0.2))
-
-def discriminator(img_in, reuse=None, keep_prob=0.5):
-    activation = lrelu
-    with tf.variable_scope("discriminator", reuse=reuse):
+def discriminator(img_in,keep_prob=0.5):
+    activation = lambda x: tf.nn.leaky_relu(x, alpha=0.02)
+    with tf.variable_scope("discriminator", reuse=tf.AUTO_REUSE):
         x = tf.reshape(img_in, shape=[-1, 128, 128, 3])
         x = tf.layers.conv2d(x, kernel_size=5, filters=32, strides=2, padding='same', activation=activation)
         x = tf.layers.dropout(x, keep_prob)
@@ -20,19 +17,19 @@ def discriminator(img_in, reuse=None, keep_prob=0.5):
         x = tf.contrib.layers.flatten(x)
         logits = tf.layers.dense(x, units=1)
         x = tf.sigmoid(logits)
-        return x , logits
+        return x, logits
 
 def generator(z, keep_prob=0.5, is_training=True):
-    activation = lrelu
+    activation = tf.nn.relu
     momentum = 0.9
     with tf.variable_scope("generator", reuse=tf.AUTO_REUSE):
         x = z
         x = tf.layers.dense(x, units=4*4*1024, activation=activation)
-        x = tf.reshape(x, shape=[-1,4,4,1024])
-        x = tf.layers.dropout(x, keep_prob)      
+        x = tf.reshape(x, shape=[-1, 4, 4, 1024])
+        x = tf.layers.dropout(x, keep_prob)
         x = tf.contrib.layers.batch_norm(x, is_training=is_training, decay=momentum)  
         x = tf.layers.conv2d_transpose(x, kernel_size=5, filters=512, strides=2, padding='same', activation=activation)
-        x = tf.layers.dropout(x, keep_prob)      
+        x = tf.layers.dropout(x, keep_prob)
         x = tf.contrib.layers.batch_norm(x, is_training=is_training, decay=momentum)  
         x = tf.layers.conv2d_transpose(x, kernel_size=5, filters=256, strides=2, padding='same', activation=activation)
         x = tf.layers.dropout(x, keep_prob)
