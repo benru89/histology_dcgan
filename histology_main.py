ace9b729ebfd10c49290a92e77bc805dd3318fd1
Ruben FB <rubenfb14@gmail.com>
2019-04-23 21:58:46 +0200

    added batch norm and removed dropout.

--- histology_main.py
+++ histology_main.py
@@ -32,7 +32,7 @@ def run():
         iterator = dataset.make_initializable_iterator()
         next_batch = iterator.get_next()
         # Model
-        input_real, input_z, _ = model.model_inputs(DIM_X, DIM_Y, DIM_Z, Z_NOISE_DIM)
+        input_real, input_z = model.model_inputs(DIM_X, DIM_Y, DIM_Z, Z_NOISE_DIM)
         d_loss, g_loss = model.model_loss(input_real, input_z, DIM_Z)
         d_train_opt, g_train_opt = model.model_opt(d_loss, g_loss, D_LEARNING_RATE, G_LEARNING_RATE, BETA1)
         
@@ -41,17 +41,19 @@ def run():
         saver = tf.train.Saver(max_to_keep=20, keep_checkpoint_every_n_hours=2)
 
         with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
             try:
                 saver.restore(sess, tf.train.latest_checkpoint(BASE_PATH + CHKPTS_PATH))
                 last_global_step = sess.run(global_step)
                 print(last_global_step)
             except:
-                sess.run(tf.global_variables_initializer())
+                print("Error loading checkpoint")
+                
             sess.run(iterator.initializer)
             writer = tf.summary.FileWriter(BASE_PATH + GRAPHS_PATH, sess.graph)
             g_loss_sum = tf.summary.scalar("g_loss", g_loss)
             d_loss_sum = tf.summary.scalar("d_loss", d_loss)
-            samples, _ = generator(input_z,is_training=False)
+            samples, _ = generator(input_z, training=False)
             g_img_sum = tf.summary.image("G", samples, max_outputs=10)
             g_sum = tf.summary.merge([g_loss_sum, g_img_sum])
             while True:
@@ -62,9 +64,9 @@ def run():
 
                     _, d_loss_sum_str = sess.run([d_train_opt, d_loss_sum], feed_dict={input_real: batch, input_z: batch_z})
                     writer.add_summary(d_loss_sum_str, steps)
-                    _, g_sum_str = sess.run([g_train_opt, g_sum], feed_dict={input_z: batch_z})
+                    _, g_sum_str = sess.run([g_train_opt, g_sum], feed_dict={input_real: batch,input_z: batch_z})
                     writer.add_summary(g_sum_str, steps)
-                    _, g_sum_str = sess.run([g_train_opt, g_sum], feed_dict={input_z: batch_z})
+                    _, g_sum_str = sess.run([g_train_opt, g_sum], feed_dict={input_real: batch,input_z: batch_z})
                     writer.add_summary(g_sum_str, steps)
                     sess.run(increment_global_step)
                                
