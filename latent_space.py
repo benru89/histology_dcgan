fbd586e7ad6f80ee5f1cb45b3541902cf6c270d8
Ruben FB <rubenfb14@gmail.com>
2019-05-13 23:53:08 +0200

    refactoring and sum and substract operations
    add operations to manipulate tensors in latent space.

--- latent_space.py
+++ latent_space.py
@@ -5,25 +5,27 @@ from dcgan_alt import sampler
 from PIL import Image
 from output import transform_image, image_from_array
 
-STEPS = 50
 
 def search_image(sess, image_to_search):
-    image_arr = (np.array(image_to_search) /127.5) - 1
+    image_arr = (np.array(image_to_search) / 127.5) - 1
     zp, zp_value = search(sess, image_arr)
     samples = sess.run(sampler(zp))
     imgs = [img[:, :, :] for img in samples]
-    img = Image.fromarray(((np.reshape(imgs[0], (DIM_X, DIM_Y, DIM_Z)) + 1) * 127.5).astype(np.uint8))
+    img = Image.fromarray(
+        ((np.reshape(imgs[0], (DIM_X, DIM_Y, DIM_Z)) + 1) * 127.5).astype(np.uint8))
     return img, zp_value
 
-def search(sess,tensor, save_tensor=False):
+
+def search(sess, tensor, save_tensor=False):
     fz = tf.Variable(tensor, tf.float32)
     fz.initializer.run(session=sess)
     fz = tf.expand_dims(fz, 0)
-    fz = tf.cast(fz,tf.float32)
-    zp = tf.Variable(np.random.normal(size=(BATCH_SIZE,Z_NOISE_DIM)), dtype=tf.float32)
+    fz = tf.cast(fz, tf.float32)
+    zp = tf.Variable(np.random.normal(
+        size=(BATCH_SIZE, Z_NOISE_DIM)), dtype=tf.float32)
     zp.initializer.run(session=sess)
     fzp = sampler(zp)
-   
+
     loss = tf.losses.mean_squared_error(labels=fz, predictions=fzp)
 
     # Decayed gradient descent
@@ -31,41 +33,49 @@ def search(sess,tensor, save_tensor=False):
     descent_step.initializer.run(session=sess)
     starter_learning_rate = 0.99
     learning_rate = tf.train.exponential_decay(starter_learning_rate,
-                                            descent_step,
-                                            10000, 0.00005)
+                                               descent_step,
+                                               10000, 0.00005)
     opt = tf.train.GradientDescentOptimizer(learning_rate)
     # Optimize on the variable zp
     train = opt.minimize(loss, var_list=zp, global_step=descent_step)
-    for i in range(1000): 
+    for i in range(1000):
         _, loss_value, zp_val, eta = sess.run((train, loss, zp, learning_rate))
         print("%03d) eta=%03f, loss = %f" % (i, eta, loss_value))
-        
+
     zp_val = sess.run(zp)
-    print(zp_val.shape)
-    print(zp_val * 0.5)
-    
     return zp, zp_val
+  
+def interpolate(sess, tensor_a, tensor_b, steps=50):
+    z = np.empty(shape=(steps, Z_NOISE_DIM))
+    for i, alpha in enumerate(np.linspace(start=0.0, stop=1.0, num=steps)):
+        z[i] = (1-alpha) * tensor_a + alpha * tensor_b
 
-def interpolate(sess, tensor_a, tensor_b):
-    z = np.empty(shape=(STEPS, Z_NOISE_DIM))
-    for i, alpha in enumerate(np.linspace(start=0.0, stop=1.0, num=STEPS)):
-       z[i] = (1-alpha) * tensor_a + alpha * tensor_b
+    z_ = tf.placeholder(tf.float32, [steps, Z_NOISE_DIM])
 
-    z_ = tf.placeholder(tf.float32, [None, Z_NOISE_DIM])
-
-    samples = sess.run(sampler(z_),feed_dict={z_: z})
+    samples = sess.run(sampler(z_,steps), feed_dict={z_: z})
     imgs = [img[:, :, :] for img in samples]
     for i, image in enumerate(imgs):
         filepath = FULL_OUTPUT_PATH + 'interp_' + str(i) + '.png'
-        output_image = Image.fromarray(transform_image(image_from_array(image)).astype(np.uint8))
+        output_image = Image.fromarray(transform_image(
+            image_from_array(image)).astype(np.uint8))
         output_image.save(filepath)
 
-def interpolate_images(sess, image_a, image_b):
+
+def interpolate_images(sess, image_a, image_b, steps=50):
     tensor_a, _ = search_image(sess, image_a)
     tensor_b, _ = search_image(sess, image_b)
-    interpolate(sess, tensor_a, tensor_b)
+    interpolate(sess, tensor_a, tensor_b, steps)
 
-def interpolate_rand(sess, image):
+
+def interpolate_rand(sess, image, steps=50):
     tensor_a, _ = search_image(sess, image)
-    rand_tensor = tf.random.uniform((1, Z_NOISE_DIM), dtype=tf.float32, minval=-1, maxval=1)
-    interpolate(sess, tensor_a, rand_tensor)
+    rand_tensor = tf.random.uniform(
+        (1, Z_NOISE_DIM), dtype=tf.float32, minval=-1, maxval=1)
+    interpolate(sess, tensor_a, rand_tensor, steps)
+
+
+def add_tensors(tensor_a, tensor_b):
+    return tensor_a + tensor_b
+
+def substract_tensors(tensor_a, tensor_b):
+    return tensor_a - tensor_b
