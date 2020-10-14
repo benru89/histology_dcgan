2be6c953025ca700ab1aa2a858421fac3c5c78bc
Ruben <rubenfb14@gmail.com>
2020-10-15 00:22:30 +0200

    remove dataset creation from get_session function
    remove dataset creation from get_session function

--- histology_main.py
+++ histology_main.py
@@ -119,11 +119,7 @@ def get_session(batch_size):
       Returns an open tf session from last valid checkpoint. Useful to use a saved trained model.
       ie. use this function in a Jupyter notebook to create a session.
     """
-    # Dataset
-    dataset, dataset_len = data.create_dataset(
-        BASE_PATH + DATA_PATH, BATCH_SIZE, NUM_EPOCHS)
-    iterator = dataset.make_initializable_iterator()
- 
+
     # Model
     input_real, input_z, input_d_y, input_g_y = model.model_inputs(
         DIM_X, DIM_Y, DIM_Z, Z_NOISE_DIM, Y_DIM)
